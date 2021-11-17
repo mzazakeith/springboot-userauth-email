8bc5246d655aa0f7a6a6923e2c473bf0b92121a6
mzazakeith <mzazakeith@gmail.com>
2021-11-17 16:19:48 +0300

    sending emails configurtions done

---
+++ src/main/java/com/example/Springsecurityemail/service/EmailService.java
@@ -0,0 +1,39 @@
+package com.example.Springsecurityemail.service;
+
+import com.example.Springsecurityemail.util.EmailSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService implements EmailSender {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try{
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
+            mimeMessageHelper.setText(email, true);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject("Confirm your email Springboot");
+            mimeMessageHelper.setFrom("emailapitest@email.com");
+            javaMailSender.send(mimeMessage);
+        }catch (MessagingException e){
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
---
+++ src/main/java/com/example/Springsecurityemail/util/EmailSender.java
@@ -0,0 +1,5 @@
+package com.example.Springsecurityemail.util;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
