e0f0f4a101e0c52f77ca397b18a4ca4c3e7858cc
mzazakeith <mzazakeith@gmail.com>
2021-11-17 12:02:44 +0300

    Initial files, dependencies and database configurations

---
+++ src/main/java/com/example/Springsecurityemail/SpringSecurityEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springsecurityemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityEmailApplication.class, args);
+	}
+
+}
